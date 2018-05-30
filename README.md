5f0b8876db4fe2c5e5022557c347ec8bfb7c4390
pspgbhu <brotherchun001@gmail.com>
2018-05-30 16:19:56 +0800

    feat: create config about templates

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # smarter-assets
 config and assets for smarter
+
+## What's this
+
+将资源文件和配置文件从 [smarter](https://github.com/jd-smart-fe/smarter) 中分离出来，实现逻辑与配置相分离。
---
+++ config/templates.js
@@ -0,0 +1,21 @@
+/**
+ * smarter init 命令下的模板库列表配置
+ *
+ * @prop {string} name 给你的脚手架起一个好听的名字
+ * @prop {string} git 模板库的 git 地址
+ * @prop {string} desc 一句话说清楚你的脚手架是干什么的
+ */
+
+module.export = [{
+  name: 'rephic',
+  git: 'https://github.com/pspgbhu/Rephic.git',
+  desc: 'React 同构直出脚手架',
+}, {
+  name: 'koa-ts-template',
+  git: 'https://github.com/jd-smart-fe/koa-ts-template.git',
+  desc: '基于 Koa 的 TypeScript 工程脚手架',
+}, {
+  name: 'hammer-site',
+  git: 'https://github.com/xieyankun/HammerSite.git',
+  desc: 'H5 静态页面工程脚手架',
+}];
