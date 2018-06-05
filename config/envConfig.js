45de85463f110174b885f6a8fd6ab18dac194caf
mengqingpan <mngqingpan@jd.com>
2018-06-05 18:20:27 +0800

    fix: 修改path报错问题

--- config/envConfig.js
+++ config/envConfig.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
 /**
  * smarter env 命令下的配置文件路径（后续可以添加其他配置文件）
  *
  * @prop {string} source 配置文件原路径
  * @prop {string} target 配置文件目标路径
  */
-
+const path = require('path');
+ 
 module.exports = {
   vscode: {
     source: path.resolve(__dirname, '../assets/env/vscodeSettings.json'),
